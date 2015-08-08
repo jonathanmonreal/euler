d7fde84feda90eee21227bbcc057ddeeaec8d8b7
jonathanmonreal <jem177@case.edu>
2015-08-07 21:49:32 -0400

    add problem_4 to euler.py

--- euler.py
+++ euler.py
@@ -25,3 +25,11 @@ def problem_2():
         previous = temp
 
     return total
+
+def problem_4():
+    largest = -1
+    for i in range(100, 1000):
+        for j in range(100, 1000):
+            if i * j > largest and str(i * j) == str(i * j)[::-1]:
+                largest = i * j
+    return largest
